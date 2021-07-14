57554129447b41d4536322e847488019ed5748d8
ValaryUhuru <valary.uhuru@nativecoders.se>
2021-07-14 10:20:32 +0300

    added code to the dashbaord, removed server folder from the same folder as the client-side app

--- server/routes/settings.js
+++
@@ -1,9 +0,0 @@
-// change this
-
-export const MONGO_DB_NAME = 'ProfileManager'
-export const MONGO_URL = 'mongodb://localhost:27017/'+MONGO_DB_NAME
-export const API_KEY = '__api_key__'
-
-
-// collection name
-export const USERS_COL = 'users'
---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import design from "../asset/images/design.png";
+import website from "../asset/images/website.png";
+import mango from "../asset/images/mango.jpeg";
+
+const projects = [
+  {
+    name: "Vauk Project",
+    picture: design,
+  },
+  {
+    name: "Cloud Shift",
+    picture: design,
+  },
+  {
+    name: " Profile Manager ",
+    picture: design,
+  }
+];
+
+export default projects;
